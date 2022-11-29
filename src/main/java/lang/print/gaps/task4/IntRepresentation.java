7a774dfafa564c52e13908166bb9fb23d971296f
Jahongir2109 <jahongirachilov004@gmail.com>
2022-11-29 16:52:28 +0500

    fixed all

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c=(char)'c';
+        int h=(char)'h';
+        int o=(char)'o';
+        int r=(char)'r';
+        System.out.println((char) c);
+        System.out.println((char) h);
+        System.out.println((char) o);
+        System.out.println((char) r);
     }
 }
